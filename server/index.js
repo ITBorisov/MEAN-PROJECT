3dd6ab309bb1f78927eba665b88ca236b36c5e8b
Georgi Borisov <georgi.borisov.it@gmail.com>
2018-07-29 20:34:52 +0300

    Basic server init

---
+++ server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config/config').get(process.env.NODE_ENV);
+
+const app = express();
+
+const userRoutes = require('./routes/user');
+const adsRoutes = require('./routes/ads');
+
+
+mongoose
+  .connect(config.DATABASE)
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+  next();
+});
+
+
+app.use('/api/user', userRoutes);
+app.use('/api/ads', adsRoutes);
+
+
+const port = process.env.PORT || 3001;
+app.listen(port,()=>{
+    console.log(`Server is running on port: ${port}`)
+})
+
+module.exports = app;
---
+++ server/routes/user.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post('', (req, res, next) => {
+    res.send('probaaa');
+})
+
+
+module.exports = router;
